83af26388d5e290595d0d5f658c47c949ed3230f
oreinaldodeoliver <oreinaldodeoliveirafilho@gmail.com>
2026-07-17 20:47:48 -0300

    aula01 prof:Cesar data:17/07/2026

---
+++ Lógica de Programação e Algoritmos/Aula01/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿double x, y, media;
+Console.WriteLine("Digite o prieiro numero: ");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("digite o segundo numero: ");
+y = double.Parse(Console.ReadLine());
+media = (x + y) / 2.0;
+Console.WriteLine("Media = " + media);
