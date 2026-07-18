c36db88f9d2bf6eea68bf37f759963a8cb34211b
oreinaldodeoliver <oreinaldodeoliveirafilho@gmail.com>
2026-07-17 21:52:39 -0300

    finalizado aula01 no dia 17/07/2026

---
+++ Lógica de Programação e Algoritmos/Aula01/saidadedados/saidadedados/saidadedados/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI";
+char caracter = 'S';
+bool logica1 = true;
+bool logica2 = false;
+
+Console.WriteLine(palavra);
+Console.WriteLine("SENAI Americana");
+Console.WriteLine("Curso DEV BackEND");
+Console.Write("Argentina");
+Console.Write("Argentina");
+ReadKey();
