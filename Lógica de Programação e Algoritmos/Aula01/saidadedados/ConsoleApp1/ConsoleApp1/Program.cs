0b5c14d0843f0bd8d33169ed6b0050a757a9e6c3
oreinaldodeoliver <oreinaldodeoliveirafilho@gmail.com>
2026-07-20 21:56:24 -0300

    Declaração de varuaveis,Processamento e saída de dados. (aula 02,SENAI

---
+++ Lógica de Programação e Algoritmos/Aula01/saidadedados/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declaração de variáveis
+double largura;
+double comprimento;
+double area;;
+double valor;
+double preco;
+
+//Entrada de dados
+Console.Write("Digite a largura do terreno: ");
+largura = int.Parse(Console.ReadLine());    
+Console.Write("Digite o comprimento do terreno:");
+comprimento = double.Parse(Console.ReadLine()); 
+Console.Write("digite o valor do metro quadrado do terreno: ");
+valor = double.Parse(Console.ReadLine());
+
+//Processamento de dados 
+area = largura * comprimento;
+preco = area * valor;
+
+//Saída de dados
+Console.WriteLine($"Área do terreno: {area}\nPreço do terreno: R$ {preco:n}");
---
+++ Lógica de Programação e Algoritmos/Aula01/saidadedados/ConsoleApp1/ExercicioRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿//Criação das variáveis
+double bAse;
+double altura;
+double area;
+double perimetro;
+double diagonal;
+
+//Entrada de dados
+Console.Write("Digite a bAse do retangulo: ");
+bAse = double.Parse(Console.ReadLine());
+Console.Write("Digite a altura do retangulo: ");
+altura = double.Parse(Console.ReadLine());
+
+//Processamento de dados
+area = bAse * altura;
+perimetro = bAse + altura + bAse + altura;
+diagonal = Math.Sqrt(Math.Pow(bAse, 2 ) +Math.Pow(altura, 22));
+
+//Saída de dados
+Console.WriteLine($"--Dados do retângulo--\n" +
+$"Área: {area:f2}\n" +
+$"Perímetro: {perimetro:f2}\n" +
+$"Diagonal: {diagonal:n}\n");
+
+
